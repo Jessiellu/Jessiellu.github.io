536d0b5b05fcc6f39a18e29749788f8c836a46a1
Jessiellu <jessiellu20@163.com>
2021-04-16 23:29:25 +0800

    commit

--- js/generateReadMe.js
+++
@@ -1,7 +0,0 @@
-var marked = require('marked');
-var fs = require('fs');
-
-var readMe = fs.readFileSync('README.md','utf-8');
-var markdownReadMe = marked(readMe);
-
-fs.writeFileSync('../README.html',markdownReadMe);
