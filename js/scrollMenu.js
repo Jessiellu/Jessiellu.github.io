6d0fec96aa86f2c0da5a6bc028e6604811f362de
Jessiellu <jessiellu20@163.com>
2023-04-21 20:36:35 +0800

    add portfolios

---
+++ js/scrollMenu.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function() {
+    //获取顶部菜单元素
+    let navBar = document.querySelector('#header__nav')
+    //获取元素高度
+    let navBarHeight = navBar.offsetHeight
+    //获取顶部菜单后面的内容，为此添加样式
+    let afterContent = document.querySelector('#header__title')
+    //获取顶部菜单距离页面顶部的距离
+    let navBarTop = navBar.offsetTop
+
+    //添加页面滚动的监听：
+})
